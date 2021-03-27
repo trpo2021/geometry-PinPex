30d1af182e221cc468bcdc406576366a8a5b17dc
PinPex <aleksei-picnhuk0202@mail.ru>
2021-03-27 16:43:01 +0700

    Adapted main source for all libraries

--- src/geometry/geometry.cpp
+++ src/geometry/geometry.cpp
@@ -1,10 +1,6 @@
 #include <conio.h>
 #include <iostream>
-#include <libgeometry/Calc.h>
-#include <libgeometry/Find.h>
-#include <libgeometry/Input.h>
 #include <libgeometry/libgeometry.h>
-#include <libgeometry/output.h>
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,29 +8,36 @@
 using namespace std;
 const double pi = 3.141592653589793238463;
 
-
-
 int main()
 {
     int n, i, j;
     double Points[6];
     double perimeter, area;
+    bool HaveCir = 0;
+    bool HaveTri = 0;
 
-	printf("How many objects you wish input?\n");
+    printf("How many objects you wish input?\n");
     n = input();
     printf("Input your objects\n");
-	
-	int** check = new int*[n];
+
     cir* circles = new cir[n];
-	tri* tr = new tri[n];
+    tri* tr = new tri[n];
+
+    for (i = 0; i < n; ++i) {
+        tr[i].intersec = new int[n];
+        tr[i].inter = new string[n];
+        circles[i].intersec = new int[n];
+        circles[i].inter = new string[n];
+    }
     string Object;
 
-    
     for (i = 0; i < n; i++) {
         getline(cin, Object);
-        circles[i].Ob = Object;
         switch (ind(Object)) {
         case (1): {
+            HaveCir = 1;
+            tr[i].Ob = " ";
+            circles[i].Ob = Object;
             findPoints(Object, Points);
             perimeter = calcParam(Points[2]);
             area = calcArea(Points[2]);
@@ -45,43 +48,40 @@ int main()
             circles[i].ar = area;
             break;
         }
-        case (2):{
-			findvert(Object, Points);
-			tr[i].Ob = Object;
-			perimeter = calcParamtriandArea(Points,area);
-			if(perimeter == -1) cout << "This triangle not exists" << endl;
-			tr[i].ar = 0;
-			tr[i].per = 0;
-			break;
-			for(j=0;j<6;++j) tr[i].ps[j] = Points[j];
-			tr[i].ar = area;
-			tr[i].per = perimeter;
-			
+        case (2): {
+            HaveTri = 1;
+            circles[i].Ob = " ";
+            findvert(Object, Points);
+            tr[i].Ob = Object;
+            perimeter = calcParamtriandArea(Points, &area);
+            if (perimeter == -1) {
+                cout << "This triangle not exists" << endl;
+                tr[i].ar = 0;
+                tr[i].per = 0;
+                break;
+            }
+            for (j = 0; j < 6; ++j)
+                tr[i].ps[j] = Points[j];
+            tr[i].ar = area;
+            tr[i].per = perimeter;
+
             break;
-		}
+        }
         case (0): {
-            cout << "Error" << endl;
+            cout << "Error at column 0: expected 'circle', 'triangle' or "
+                    "'polygon'"
+                 << endl;
             break;
         }
         }
     }
 
-    for (i = 0; i < n; ++i) {
-        cout << "Point 1 = " << circles[i].p1 << endl;
-        cout << "Point 2 = " << circles[i].p2 << endl;
-        cout << "Perimeter = " << circles[i].per << endl;
-        cout << "Area = " << circles[i].ar << endl;
-    }
-	for (i = 0; i < n; ++i) {
-		if(tr[i].per == 0 && tr[i].ar == 0) continue;
-        for(j=0;j<n;++j) printf("Point[%d] = %f",j + 1, tr[i].ps[j]);
-		printf("Perimeter[%d] = %f",i + 1, tr[i].per);
-		printf("Area[%d] = %f",i + 1, tr[i].ar);
-    }
-	
-    for (i = 0; i < n; ++i) {
-        cout << check[i][0] << endl;
-    }
+    if (HaveCir == 1)
+        checkCircCirc(circles, n);
+    if (HaveTri == 1)
+        checkCircTri(circles, tr, n);
+    output(circles, tr, n);
+
     system("pause");
     return 0;
 }
