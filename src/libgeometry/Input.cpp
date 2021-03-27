1b265ba81a2aa4b9a94a4ff6bfaafaa2f96fc67c
PinPex <aleksei-picnhuk0202@mail.ru>
2021-03-27 16:41:06 +0700

    Fixed bug with getline

---
+++ src/libgeometry/Input.cpp
@@ -0,0 +1,12 @@
+#include "libgeometry.h"
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int input()
+{
+    string in;
+    getline(cin, in);
+    int n = stoi(in);
+    return n;
+}
