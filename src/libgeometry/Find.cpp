06d05176748d4b3895d9ad0a4607a27192a063ab
PinPex <aleksei-picnhuk0202@mail.ru>
2021-03-27 16:39:57 +0700

    Realised find for triangle and add errorcheck

--- src/libgeometry/Find.cpp
+++ src/libgeometry/Find.cpp
@@ -1,4 +1,5 @@
 #include "libgeometry.h"
+#include <cerrno>
 #include <iostream>
 #include <stdlib.h>
 #include <string.h>
@@ -10,24 +11,179 @@ void findPoints(string Object, double* Points)
 {
     int i = 7;
     int j = i;
-    while (Object[j] != ' ')
+    if (Object[i - 1] == ')') {
+        printf("Error at column %d : expected '('\n", i - 1);
+        system("pause");
+        exit(0);
+    }
+    while (Object[j] != ' ') {
         j += 1;
-    j -= 1;
-    Points[0] = atof(Object.substr(i, j).c_str());
-    i = j + 1;
-    while (Object[i] == ' ')
-        i += 1;
-    j = i;
-    while (Object[j] != ',')
+    }
+    if ((strtod(Object.substr(i, j - 1).c_str(), NULL) == 0)
+        && (Object.substr(i, j - 1) != "0")
+        && (Object.substr(i, j - 1) != "0.0")) {
+        printf("Error at column %d: expected '<double>'\n", i - 1);
+        system("pause");
+        exit(0);
+    }
+    Points[0] = strtod(Object.substr(i, j - 1).c_str(), NULL);
+    while (Object[j] == ' ') {
         j += 1;
-    j -= 1;
-    Points[1] = atof(Object.substr(i, j).c_str());
-    j += 2;
+    }
     i = j;
-    while (Object[i] == ' ')
-        i += 1;
-    j = i;
-    while (Object[j] != ')')
+    while (Object[j] != ',') {
         j += 1;
-    Points[2] = atof(Object.substr(i, j).c_str());
+    }
+    if ((strtod(Object.substr(i, j - 1).c_str(), NULL) == 0)
+        && (Object.substr(i, j - 1) != "0")
+        && (Object.substr(i, j - 1) != "0.0")) {
+        printf("Error at column %d: expected '<double>'\n", i - 1);
+        system("pause");
+        exit(0);
+    }
+    Points[1] = strtod(Object.substr(i, j - 1).c_str(), NULL);
+    j += 1;
+    while (Object[j] == ' ') {
+        j += 1;
+    }
+    i = j;
+    while (((Object[j] != ')') ^ (Object[j] != '(')) == 1) {
+        j += 1;
+    }
+    if ((strtod(Object.substr(i, j - 1).c_str(), NULL) == 0)
+        && (Object.substr(i, j - 1) != "0")
+        && (Object.substr(i, j - 1) != "0.0")) {
+        printf("Error at column %d: expected '<double>'\n", i - 1);
+        system("pause");
+        exit(0);
+    }
+    Points[2] = strtod(Object.substr(i, j - 1).c_str(), NULL);
+
+    if (Object[j] == '(') {
+        printf("Error at column %d : expected ')'\n", j);
+        system("pause");
+        exit(0);
+    }
+
+    i = Object.length();
+    if (i != j + 2) {
+        while (Object[j] == ' ') {
+            j += 1;
+        }
+        printf("Error at column %d: unexpected token\n", j);
+        system("pause");
+        exit(0);
+    }
+}
+void findvert(string Object, double* Points)
+{
+    int i, j;
+    j = i = 9;
+    if (Object[i] == ')') {
+        printf("Error at column %d : expected '('\n", i - 1);
+        system("pause");
+        exit(0);
+    }
+    while (Object[j] != ' ') {
+        j += 1;
+    }
+    if ((strtod(Object.substr(i, j - 1).c_str(), NULL) == 0)
+        && (Object.substr(i, j - 1) != "0")
+        && (Object.substr(i, j - 1) != "0.0")) {
+        printf("Error at column %d: expected '<double>'\n", i - 1);
+        system("pause");
+        exit(0);
+    }
+    Points[0] = strtod(Object.substr(i, j - 1).c_str(), NULL);
+    while (Object[j] == ' ') {
+        j += 1;
+    }
+    i = j;
+    while (Object[j] != ',') {
+        j += 1;
+    }
+    if ((strtod(Object.substr(i, j - 1).c_str(), NULL) == 0)
+        && (Object.substr(i, j - 1) != "0")
+        && (Object.substr(i, j - 1) != "0.0")) {
+        printf("Error at column %d: expected '<double>'\n", i - 1);
+        system("pause");
+        exit(0);
+    }
+    Points[1] = strtod(Object.substr(i, j - 1).c_str(), NULL);
+    j += 1;
+    while (Object[j] == ' ') {
+        j += 1;
+    }
+    i = j;
+    while (Object[j] != ' ') {
+        j += 1;
+    }
+    if ((strtod(Object.substr(i, j - 1).c_str(), NULL) == 0)
+        && (Object.substr(i, j - 1) != "0")
+        && (Object.substr(i, j - 1) != "0.0")) {
+        printf("Error at column %d: expected '<double>'\n", i - 1);
+        system("pause");
+        exit(0);
+    }
+    Points[2] = strtod(Object.substr(i, j - 1).c_str(), NULL);
+    while (Object[j] == ' ') {
+        j += 1;
+    }
+    i = j;
+    while (Object[j] != ',') {
+        j += 1;
+    }
+    if ((strtod(Object.substr(i, j - 1).c_str(), NULL) == 0)
+        && (Object.substr(i, j - 1) != "0")
+        && (Object.substr(i, j - 1) != "0.0")) {
+        printf("Error at column %d: expected '<double>'\n", i - 1);
+        system("pause");
+        exit(0);
+    }
+    Points[3] = strtod(Object.substr(i, j - 1).c_str(), NULL);
+    j += 1;
+    while (Object[j] == ' ') {
+        j += 1;
+    }
+    i = j;
+    while (Object[j] != ' ') {
+        j += 1;
+    }
+    if ((strtod(Object.substr(i, j - 1).c_str(), NULL) == 0)
+        && (Object.substr(i, j - 1) != "0")
+        && (Object.substr(i, j - 1) != "0.0")) {
+        printf("Error at column %d: expected '<double>'\n", i - 1);
+        system("pause");
+        exit(0);
+    }
+    Points[4] = strtod(Object.substr(i, j - 1).c_str(), NULL);
+    while (Object[j] == ' ') {
+        j += 1;
+    }
+    i = j;
+    while (((Object[j] != ')') ^ (Object[j] != '(')) == 1) {
+        j += 1;
+    }
+    if ((strtod(Object.substr(i, j - 1).c_str(), NULL) == 0)
+        && (Object.substr(i, j - 1) != "0")
+        && (Object.substr(i, j - 1) != "0.0")) {
+        printf("Error at column %d: expected '<double>'\n", i - 1);
+        system("pause");
+        exit(0);
+    }
+    Points[5] = strtod(Object.substr(i, j - 1).c_str(), NULL);
+    if (Object[j] == '(') {
+        printf("Error at column %d : expected ')'\n", j);
+        system("pause");
+        exit(0);
+    }
+    i = Object.length();
+    if (i != j + 2) {
+        while (Object[j] == ' ') {
+            j += 1;
+        }
+        printf("Error at column %d: unexpected token\n", j);
+        system("pause");
+        exit(0);
+    }
 }
