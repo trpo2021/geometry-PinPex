33c28896bc838bdc735edc14f7743c778c320747
PinPex <aleksei-picnhuk0202@mail.ru>
2021-04-23 16:42:05 +0700

    Made build and followed codestyle

--- src/libgeometry/Find.cpp
+++ src/libgeometry/Find.cpp
@@ -13,51 +13,51 @@ int findPoints(string Object, double* Points)
     if (Object[i - 1] == ')') {
         printf("Error at column %d : expected '('\n", i - 1);
         system("pause");
-		return 101;
+        return 101;
         exit(101);
     }
-	if (Object[i-1] == ' ') {
+    if (Object[i - 1] == ' ') {
         printf("Error at column %d : a few spaces for circle\n", i - 1);
         system("pause");
-		return 202;
+        return 202;
         exit(202);
     }
     while (Object[j] != ' ') {
         j += 1;
     }
-	try{
-    Points[0] = strtod(Object.substr(i, j - 1).c_str(), NULL);
-	}
-	catch(const std::out_of_range& e){
-		printf("Error at column %d: expected '<double>'\n", i);
+    try {
+        Points[0] = strtod(Object.substr(i, j - 1).c_str(), NULL);
+    } catch (const std::out_of_range& e) {
+        printf("Error at column %d: expected '<double>'\n", i);
         system("pause");
-		return 303;
+        return 303;
         exit(303);
-	}
+    }
     while (Object[j] == ' ') {
         j += 1;
     }
     i = j;
     while (1) {
         j += 1;
-		if(Object[j] == ',') break;
-		if(Object[j] == ' ') break;
+        if (Object[j] == ',')
+            break;
+        if (Object[j] == ' ')
+            break;
     }
-	if(Object[j] == ' '){
-		printf("Error at column %d: a few spaces for circle\n", i);
+    if (Object[j] == ' ') {
+        printf("Error at column %d: a few spaces for circle\n", i);
         system("pause");
-		return 202;
-        exit(202);		
-	}
-    try{
-    Points[1] = strtod(Object.substr(i, j - 1).c_str(), NULL);
-	}
-	catch(const std::out_of_range& e){
-		printf("Error at column %d: expected '<double>'\n", i);
+        return 202;
+        exit(202);
+    }
+    try {
+        Points[1] = strtod(Object.substr(i, j - 1).c_str(), NULL);
+    } catch (const std::out_of_range& e) {
+        printf("Error at column %d: expected '<double>'\n", i);
         system("pause");
-		return 303;
+        return 303;
         exit(303);
-	}
+    }
     j += 1;
     while (Object[j] == ' ') {
         j += 1;
@@ -65,100 +65,101 @@ int findPoints(string Object, double* Points)
     i = j;
     while (1) {
         j += 1;
-		if(Object[j]==')') break;
-		if(Object[j]=='(') break;
-		if(Object[j]==',') break;
-		if(Object[j]==' ') break;
+        if (Object[j] == ')')
+            break;
+        if (Object[j] == '(')
+            break;
+        if (Object[j] == ',')
+            break;
+        if (Object[j] == ' ')
+            break;
     }
-	if(Object[j]==' ' || Object[j]==','){
-		printf("Error at column %d: a few elements for circle\n", j);
+    if (Object[j] == ' ' || Object[j] == ',') {
+        printf("Error at column %d: a few elements for circle\n", j);
         system("pause");
-		return 404;
+        return 404;
         exit(404);
-	}
-    try{
-    Points[2] = strtod(Object.substr(i, j - 1).c_str(), NULL);
-	}
-	catch(const std::out_of_range& e){
-		printf("Error at column %d: expected '<double>'\n", i);
+    }
+    try {
+        Points[2] = strtod(Object.substr(i, j - 1).c_str(), NULL);
+    } catch (const std::out_of_range& e) {
+        printf("Error at column %d: expected '<double>'\n", i);
         system("pause");
-		return 303;
+        return 303;
         exit(303);
-	}
+    }
     if (Object[j] == '(') {
         printf("Error at column %d : expected ')'\n", j);
         system("pause");
-		return 505;
+        return 505;
         exit(505);
     }
-	i = Object.length()-1;
-    if (Object[j] == ')' && (i!=j)) {
-		j+=1;
+    i = Object.length() - 1;
+    if (Object[j] == ')' && (i != j)) {
+        j += 1;
         while (Object[j] == ' ') {
             j += 1;
         }
         printf("Error at column %d: unexpected token\n", j);
         system("pause");
-		return 606;
+        return 606;
         exit(606);
     }
-	return 0;
+    return 0;
 }
-
-
 
 int findvert(string Object, double* Points)
 {
     int i, j;
     j = i = 9;
-    if (Object[i-1] == ')') {
+    if (Object[i - 1] == ')') {
         printf("Error at column %d : expected '('\n", i - 1);
         system("pause");
-		return 101;
+        return 101;
         exit(101);
     }
-	if (Object[i-1] == ' ') {
+    if (Object[i - 1] == ' ') {
         printf("Error at column %d : a few spaces for triangle\n", i - 1);
         system("pause");
-		return 202;
+        return 202;
         exit(202);
     }
     while (Object[j] != ' ') {
         j += 1;
     }
-	try{
-    Points[0] = strtod(Object.substr(i, j - 1).c_str(), NULL);
-	}
-	catch(const std::out_of_range& e){
-		printf("Error at column %d: expected '<double>'\n", i);
+    try {
+        Points[0] = strtod(Object.substr(i, j - 1).c_str(), NULL);
+    } catch (const std::out_of_range& e) {
+        printf("Error at column %d: expected '<double>'\n", i);
         system("pause");
-		return 303;
+        return 303;
         exit(303);
-	}
+    }
     while (Object[j] == ' ') {
         j += 1;
     }
     i = j;
     while (1) {
         j += 1;
-		if(Object[j] == ',') break;
-		if(Object[j] == ' ') break;
+        if (Object[j] == ',')
+            break;
+        if (Object[j] == ' ')
+            break;
     }
-	if(Object[j] == ' '){
-		printf("Error at column %d: a few spaces for triangle\n", i);
+    if (Object[j] == ' ') {
+        printf("Error at column %d: a few spaces for triangle\n", i);
         system("pause");
-		return 202;
-        exit(202);		
-	}
-    try{
-    Points[1] = strtod(Object.substr(i, j - 1).c_str(), NULL);
-	}
-	catch(const std::out_of_range& e){
-		printf("Error at column %d: expected '<double>'\n", i);
+        return 202;
+        exit(202);
+    }
+    try {
+        Points[1] = strtod(Object.substr(i, j - 1).c_str(), NULL);
+    } catch (const std::out_of_range& e) {
+        printf("Error at column %d: expected '<double>'\n", i);
         system("pause");
-		return 303;
+        return 303;
         exit(303);
-	}
+    }
     j += 1;
     while (Object[j] == ' ') {
         j += 1;
@@ -167,39 +168,39 @@ int findvert(string Object, double* Points)
     while (Object[j] != ' ') {
         j += 1;
     }
-    try{
-    Points[2] = strtod(Object.substr(i, j - 1).c_str(), NULL);
-	}
-	catch(const std::out_of_range& e){
-		printf("Error at column %d: expected '<double>'\n", i);
+    try {
+        Points[2] = strtod(Object.substr(i, j - 1).c_str(), NULL);
+    } catch (const std::out_of_range& e) {
+        printf("Error at column %d: expected '<double>'\n", i);
         system("pause");
-		return 303;
+        return 303;
         exit(303);
-	}
+    }
     while (Object[j] == ' ') {
         j += 1;
     }
     i = j;
     while (1) {
         j += 1;
-		if(Object[j] == ',') break;
-		if(Object[j] == ' ') break;
+        if (Object[j] == ',')
+            break;
+        if (Object[j] == ' ')
+            break;
     }
-	if(Object[j] == ' '){
-		printf("Error at column %d: a few spaces for circle\n", i);
+    if (Object[j] == ' ') {
+        printf("Error at column %d: a few spaces for circle\n", i);
         system("pause");
-		return 202;
-        exit(202);		
-	}
-    try{
-    Points[3] = strtod(Object.substr(i, j - 1).c_str(), NULL);
-	}
-	catch(const std::out_of_range& e){
-		printf("Error at column %d: expected '<double>'\n", i - 1);
+        return 202;
+        exit(202);
+    }
+    try {
+        Points[3] = strtod(Object.substr(i, j - 1).c_str(), NULL);
+    } catch (const std::out_of_range& e) {
+        printf("Error at column %d: expected '<double>'\n", i - 1);
         system("pause");
-		return 303;
+        return 303;
         exit(303);
-	}
+    }
     j += 1;
     while (Object[j] == ' ') {
         j += 1;
@@ -208,57 +209,59 @@ int findvert(string Object, double* Points)
     while (Object[j] != ' ') {
         j += 1;
     }
-    try{
-    Points[4] = strtod(Object.substr(i, j - 1).c_str(), NULL);
-	}
-	catch(const std::out_of_range& e){
-		printf("Error at column %d: expected '<double>'\n", i - 1);
+    try {
+        Points[4] = strtod(Object.substr(i, j - 1).c_str(), NULL);
+    } catch (const std::out_of_range& e) {
+        printf("Error at column %d: expected '<double>'\n", i - 1);
         system("pause");
-		return 303;
+        return 303;
         exit(303);
-	}
+    }
     while (Object[j] == ' ') {
         j += 1;
     }
     i = j;
     while (1) {
         j += 1;
-		if(Object[j]==')') break;
-		if(Object[j]=='(') break;
-		if(Object[j]==',') break;
-		if(Object[j]==' ') break;
+        if (Object[j] == ')')
+            break;
+        if (Object[j] == '(')
+            break;
+        if (Object[j] == ',')
+            break;
+        if (Object[j] == ' ')
+            break;
     }
-	if(Object[j]==' ' || Object[j]==','){
-		printf("Error at column %d: a few elements for triangle\n", j);
+    if (Object[j] == ' ' || Object[j] == ',') {
+        printf("Error at column %d: a few elements for triangle\n", j);
         system("pause");
-		return 404;
+        return 404;
         exit(404);
-	}
-    try{
-    Points[5] = strtod(Object.substr(i, j - 1).c_str(), NULL);
-	}
-	catch(const std::out_of_range& e){
-		printf("Error at column %d: expected '<double>'\n", i - 1);
+    }
+    try {
+        Points[5] = strtod(Object.substr(i, j - 1).c_str(), NULL);
+    } catch (const std::out_of_range& e) {
+        printf("Error at column %d: expected '<double>'\n", i - 1);
         system("pause");
-		return 202;
+        return 202;
         exit(202);
-	}
+    }
     if (Object[j] == '(') {
         printf("Error at column %d : expected ')'\n", j);
         system("pause");
-		return 505;
+        return 505;
         exit(505);
     }
-    i = Object.length()-1;
-    if (Object[j] == ')' && (i!=j)) {
-		j+=1;
+    i = Object.length() - 1;
+    if (Object[j] == ')' && (i != j)) {
+        j += 1;
         while (Object[j] == ' ') {
             j += 1;
         }
         printf("Error at column %d: unexpected token\n", j);
         system("pause");
-		return 505;
+        return 505;
         exit(606);
     }
-	return 0;
+    return 0;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,18 +1,21 @@
-#include <ctest.h>
 #include <cmath>
+#include <ctest.h>
 #include <iostream>
 #include <libgeometry/libgeometry.h>
 #include <string>
 
 CTEST(Find_points, find)
 {
-    string str("circle(84621856.234841231 84621856.234841231, 84621856.234841231)");
+    string str(
+            "circle(84621856.234841231 84621856.234841231, "
+            "84621856.234841231)");
 
     double Points[3];
 
     findPoints(str, Points);
 
-    const double Point1[3] = {84621856.234841231, 84621856.234841231, 84621856.234841231};
+    const double Point1[3]
+            = {84621856.234841231, 84621856.234841231, 84621856.234841231};
 
     double temp1, temp2;
     temp1 = Point1[0];
@@ -26,42 +29,58 @@ CTEST(Find_points, find)
     ASSERT_DBL_NEAR(temp1, temp2);
     system("pause");
 }
-CTEST(Find_points, checking_for_correctness){
-	double Points[6];
-	string circle1("circle( 1 1, 1)");
-	string circle2("circle)1 1, 1)");
-	string circle3("circle(1 1, 1 1, 1)");
-	string circle4("circle(1 1, 1) 2124124");
-	string circle5("circle(x x, 1)");
-	string circle6("circle(1 1, 1(");
-	if(findPoints(circle1,Points) == 202) ASSERT_TRUE(1);
-	if(findPoints(circle2,Points) == 101) ASSERT_TRUE(1);
-	if(findPoints(circle3,Points) == 404) ASSERT_TRUE(1);
-	if(findPoints(circle4,Points) == 606) ASSERT_TRUE(1);
-	if(findPoints(circle4,Points) == 505) ASSERT_TRUE(1);
+CTEST(Find_points, checking_for_correctness)
+{
+    double Points[6];
+    string circle1("circle( 1 1, 1)");
+    string circle2("circle)1 1, 1)");
+    string circle3("circle(1 1, 1 1, 1)");
+    string circle4("circle(1 1, 1) 2124124");
+    string circle5("circle(x x, 1)");
+    string circle6("circle(1 1, 1(");
+    if (findPoints(circle1, Points) == 202)
+        ASSERT_TRUE(1);
+    if (findPoints(circle2, Points) == 101)
+        ASSERT_TRUE(1);
+    if (findPoints(circle3, Points) == 404)
+        ASSERT_TRUE(1);
+    if (findPoints(circle4, Points) == 606)
+        ASSERT_TRUE(1);
+    if (findPoints(circle4, Points) == 505)
+        ASSERT_TRUE(1);
 }
 CTEST(correctly_input, title)
 {
-	string str1circle("circle(1 1, 1)");
-	string str2circle("crrcle(1 1, 1)");
-	string str3circle("cir(1 1, 1)");
-	string str4circle("rcle(1 1, 1)");
-	string str5circle("cirrle(1 1, 1)");
-	if(ind(str1circle)==1) ASSERT_TRUE(1);
-	if(ind(str2circle)==0) ASSERT_TRUE(1);
-	if(ind(str3circle)==0) ASSERT_TRUE(1);
-	if(ind(str4circle)==0) ASSERT_TRUE(1);
-	if(ind(str5circle)==0) ASSERT_TRUE(1);
-	string str1triangle("triangle(1 1, 1 1, 1 1)");
-	string str2triangle("trrangle(1 1, 1 1, 1 1)");
-	string str3triangle("trrrngle(1 1, 1 1, 1 1)");
-	string str4triangle("iangle(1 1, 1 1, 1 1)");
-	string str5triangle("trian(1 1, 1 1, 1 1)");
-	if(ind(str1triangle)==2) ASSERT_TRUE(1);
-	if(ind(str2triangle)==0) ASSERT_TRUE(1);
-	if(ind(str3triangle)==0) ASSERT_TRUE(1);
-	if(ind(str4triangle)==0) ASSERT_TRUE(1);
-	if(ind(str5triangle)==0) ASSERT_TRUE(1);
+    string str1circle("circle(1 1, 1)");
+    string str2circle("crrcle(1 1, 1)");
+    string str3circle("cir(1 1, 1)");
+    string str4circle("rcle(1 1, 1)");
+    string str5circle("cirrle(1 1, 1)");
+    if (ind(str1circle) == 1)
+        ASSERT_TRUE(1);
+    if (ind(str2circle) == 0)
+        ASSERT_TRUE(1);
+    if (ind(str3circle) == 0)
+        ASSERT_TRUE(1);
+    if (ind(str4circle) == 0)
+        ASSERT_TRUE(1);
+    if (ind(str5circle) == 0)
+        ASSERT_TRUE(1);
+    string str1triangle("triangle(1 1, 1 1, 1 1)");
+    string str2triangle("trrangle(1 1, 1 1, 1 1)");
+    string str3triangle("trrrngle(1 1, 1 1, 1 1)");
+    string str4triangle("iangle(1 1, 1 1, 1 1)");
+    string str5triangle("trian(1 1, 1 1, 1 1)");
+    if (ind(str1triangle) == 2)
+        ASSERT_TRUE(1);
+    if (ind(str2triangle) == 0)
+        ASSERT_TRUE(1);
+    if (ind(str3triangle) == 0)
+        ASSERT_TRUE(1);
+    if (ind(str4triangle) == 0)
+        ASSERT_TRUE(1);
+    if (ind(str5triangle) == 0)
+        ASSERT_TRUE(1);
     system("pause");
 }
 CTEST(intersections, circles_without_find)
@@ -74,8 +93,8 @@ CTEST(intersections, circles_without_find)
     circles[0].r = 1;
 
     circles[1].Ob = "1";
-    circles[1].p1 = 1 + sqrt(2)/2;
-    circles[1].p2 = 1 + sqrt(2)/2;
+    circles[1].p1 = 1 + sqrt(2) / 2;
+    circles[1].p2 = 1 + sqrt(2) / 2;
     circles[1].r = 1;
 
     const int n = 2;
@@ -90,17 +109,19 @@ CTEST(intersections, circles_without_find)
     int temp1 = circles[0].intersec[1], temp2 = circles[1].intersec[0];
 
     ASSERT_EQUAL(temp1, temp2);
-	delete[] circles[0].intersec;
-	delete[] circles[0].inter;
-	delete[] circles[1].intersec;
-	delete[] circles[1].inter;
+    delete[] circles[0].intersec;
+    delete[] circles[0].inter;
+    delete[] circles[1].intersec;
+    delete[] circles[1].inter;
     system("pause");
 }
 CTEST(intersection, circles_with_find)
 {
     cir circles[2];
     string str1("circle(1 1, 1)");
-    string str2("circle(1.707106781186547524400844362105 1.707106781186547524400844362105, 1)");
+    string str2(
+            "circle(1.707106781186547524400844362105 "
+            "1.707106781186547524400844362105, 1)");
     double Points[3];
 
     findPoints(str1, Points);
@@ -126,18 +147,20 @@ CTEST(intersection, circles_with_find)
     int temp1 = circles[0].intersec[1], temp2 = circles[1].intersec[0];
 
     ASSERT_EQUAL(temp1, temp2);
-	delete[] circles[0].intersec;
-	delete[] circles[0].inter;
-	delete[] circles[1].intersec;
-	delete[] circles[1].inter;
+    delete[] circles[0].intersec;
+    delete[] circles[0].inter;
+    delete[] circles[1].intersec;
+    delete[] circles[1].inter;
     system("pause");
 }
 CTEST(intersections, triangles_and_circles)
 {
-	string circle = "circle(1 1, 1)";
-	string triangle = "triangle(6.82842712475 0, 0 6.82842712475, 6.82842712475 6.82842712475)";
-	int i, j;
-	const int n = 2;
+    string circle = "circle(1 1, 1)";
+    string triangle
+            = "triangle(6.82842712475 0, 0 6.82842712475, 6.82842712475 "
+              "6.82842712475)";
+    int i, j;
+    const int n = 2;
     double Points[6];
     double perimeter, area;
     bool HaveCir = 0;
@@ -155,10 +178,10 @@ CTEST(intersections, triangles_and_circles)
     string Object;
 
     for (i = 0; i < n; i++) {
-		if(i==0)
-        Object = circle;
-		if(i==1)
-		Object = triangle;
+        if (i == 0)
+            Object = circle;
+        if (i == 1)
+            Object = triangle;
         switch (ind(Object)) {
         case (1): {
             HaveCir = 1;
@@ -199,16 +222,16 @@ CTEST(intersections, triangles_and_circles)
         checkCircCirc(circles, n);
     if (HaveTri == 1)
         checkCircTri(circles, tr, n);
-	int temp1 = tr[1].intersec[0], temp2 = circles[0].intersec[1];
-	ASSERT_EQUAL(temp1, temp2);
-	delete[] circles;
-	delete[] tr;
+    int temp1 = tr[1].intersec[0], temp2 = circles[0].intersec[1];
+    ASSERT_EQUAL(temp1, temp2);
+    delete[] circles;
+    delete[] tr;
     system("pause");
 }
 CTEST(Calculating_circle, area_and_perimeter)
 {
     const double Number = 1389.10256;
-	ASSERT_DBL_NEAR(calcParam(Number), 8727.98879516);
-	ASSERT_DBL_NEAR(calcArea(Number), 6062035.7895);
+    ASSERT_DBL_NEAR(calcParam(Number), 8727.98879516);
+    ASSERT_DBL_NEAR(calcArea(Number), 6062035.7895);
     system("pause");
 }
